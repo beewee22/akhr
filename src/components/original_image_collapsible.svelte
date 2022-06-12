9da0b3266116f634ead5904177bbc83333d81ef2
beewee22 <beewee22@gmail.com>
2022-06-12 22:33:03 +0900

    add image header

---
+++ src/components/original_image_collapsible.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+	export let src: string;
+	let isOpen: boolean;
+</script>
+
+<details accesskey="originalImage" class="originalImageDetail" bind:open={isOpen}>
+	<summary>
+		<h2>{src}</h2>
+		<span class="originalImageToggle">
+			{isOpen ? 'Hide' : 'Show'}
+		</span>
+	</summary>
+	<a href={src} target="_blank"><img class="originalImage" loading="lazy" {src} alt={src} /></a>
+</details>
+
+<style lang="scss">
+	.originalImageDetail {
+		white-space: nowrap;
+		cursor: pointer;
+		user-select: none;
+		background-color: #eee;
+		margin: 0 0 2rem 0;
+		summary {
+			display: flex;
+			align-items: center;
+			justify-content: space-between;
+			&::marker {
+				display: inline-flex;
+			}
+			& > h2 {
+				padding: 0 0.5rem;
+				word-break: break-all;
+				text-overflow: ellipsis;
+				overflow: hidden;
+			}
+			.originalImageToggle {
+				min-width: 6rem;
+				text-align: center;
+			}
+		}
+		.originalImage {
+			width: 100%;
+		}
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import OriginalImageCollapsible from '../components/original_image_collapsible.svelte';
 	import CardEditor from './card_editor/CardCutter.svelte';
 
 	let width: number;
@@ -10,7 +11,6 @@
 		src: 'img/httpcloud3steamusercontentcomugc960860341956229149FC980D306FA8FE74C552981167CBDF4305821B31.jpg'
 	};
 </script>
-
 <div>
 	<CardEditor {...canvasInformation} bind:width bind:height />
 </div>
