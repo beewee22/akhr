15c02eca123621ef950e1929ccfa2629f02aaf69
zerodice0 <zerodice0@gmail.com>
2022-06-13 13:33:38 +0900

    이미지 수정과 관련된 로직 추가

--- src/routes/card_editor/CardCutter.svelte
+++ src/routes/card_editor/CardCutter.svelte
@@ -15,6 +15,20 @@ import { CardPositionStore } from '$lib/store';
 	let isImageLoaded = false;
 	let canvas: HTMLCanvasElement;
 	let cards: CardPositionClass[] = [];
+	let adjustSourceOffsetX = 0;
+	let adjustSourceOffsetY = 0;
+	let isDisplayBlankTitle = false;
+	let adjustBlankTitleWidth = 68;
+	let adjustBlankTitleHeight = 67;
+	let adjustBlankTitleOffsetX = -33;
+	let adjustBlankTitleOffsetY = 40;
+	let adjustBlankTitleFilterSapia = 0;
+	let adjustBlankTitleFilterSaturate = 0;
+	let isDisplayBlankDialog = false;
+	let adjustBlankDialogWidth = 67;
+	let adjustBlankDialogHeight = 86;
+	let adjustBlankDialogOffsetX = 0;
+	let adjustBlankDialogOffsetY = 41;
 
 	$: zeroBasedIndex = index - 1;
 	$: maxCardIndex = columns * rows;
@@ -35,7 +49,23 @@ import { CardPositionStore } from '$lib/store';
 	$: {
 		if (browser && isImageLoaded) {
 			requestAnimationFrame(() => {
-				renderCard(zeroBasedIndex);
+				renderCard(
+					zeroBasedIndex,
+					adjustSourceOffsetX,
+					adjustSourceOffsetY,
+					adjustBlankTitleWidth,
+					adjustBlankTitleHeight,
+					adjustBlankTitleOffsetX,
+					adjustBlankTitleOffsetY,
+					adjustBlankTitleFilterSapia,
+					adjustBlankTitleFilterSaturate,
+					adjustBlankDialogWidth,
+					adjustBlankDialogHeight,
+					adjustBlankDialogOffsetX,
+					adjustBlankDialogOffsetY,
+					isDisplayBlankTitle,
+					isDisplayBlankDialog
+				);
 			});
 		}
 	}
@@ -73,12 +103,66 @@ import { CardPositionStore } from '$lib/store';
 		});
 	};
 
-	const renderCard = (zeroBasedIndex: number) => {
+	const renderCard = (
+		zeroBasedIndex: number,
+		adjustSourceOffsetX: number,
+		adjustSourceOffsetY: number,
+		adjustBlankTitleWidth: number,
+		adjustBlankTitleHeight: number,
+		adjustBlankTitleOffsetX: number,
+		adjustBlankTitleOffsetY: number,
+		adjustBlankTitleFilterSapia: number,
+		adjustBlankTitleFilterSaturate: number,
+		adjustBlankDialogWidth: number,
+		adjustBlankDialogHeight: number,
+		adjustBlankDialogOffsetX: number,
+		adjustBlankDialogOffsetY: number,
+		isRenderBlankTitle: boolean,
+		isRenderBlankDialog: boolean
+		) => {
 		const ctx = canvas.getContext('2d') as CanvasRenderingContext2D;
 		const x = Math.floor(zeroBasedIndex % columns) * cardWidth;
 		const y = Math.floor(zeroBasedIndex / columns) * cardHeight;
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
-		ctx.drawImage(imgElement, -x, -y);
+		ctx.drawImage(
+			imgElement,
+			adjustSourceOffsetX-x,
+			-y-adjustSourceOffsetY
+		);
+
+		if (isRenderBlankTitle) {
+			const blankTitle = new Image();
+			blankTitle.src = '/resources/blank_cards/blank_title_2.png';
+			blankTitle.addEventListener('load', () => {
+				const titleCanvas = document.createElement('canvas');
+				const titleContext = titleCanvas.getContext('2d') as CanvasRenderingContext2D;
+				titleCanvas.width = blankTitle.width;
+				titleCanvas.height = blankTitle.height;
+				titleContext.filter =`saturate(${adjustBlankTitleFilterSaturate}%) sepia(${adjustBlankTitleFilterSapia}%)`;
+				titleContext.drawImage(
+					blankTitle, 
+					0, 0, blankTitle.width, blankTitle.height,
+					0, 0, titleCanvas.width, titleCanvas.height);
+	
+				ctx.drawImage(
+					titleCanvas,
+					adjustBlankTitleOffsetX, -adjustBlankTitleOffsetY,
+					canvas.width+adjustBlankTitleWidth, canvas.height+adjustBlankTitleHeight
+				);
+			});
+		}
+
+		if (isRenderBlankDialog) {
+			const blankDialog = new Image();
+			blankDialog.src = '/resources/blank_cards/blank_dialog_1.png';
+			blankDialog.addEventListener('load', () => {
+				ctx.drawImage(
+					blankDialog, 
+					adjustBlankDialogOffsetX-(adjustBlankDialogWidth/2), -adjustBlankDialogOffsetY, 
+					canvas.width+adjustBlankDialogWidth, canvas.height+adjustBlankDialogHeight
+				);
+			});
+		}
 	};
 
 	const saveImages = () => {
@@ -101,12 +185,83 @@ import { CardPositionStore } from '$lib/store';
 	<div>
 		<button class="cutBtn" on:click={saveImages}>Cut images</button>
 	</div>
-	<canvas
-		bind:this={canvas}
-		width={cardWidth}
-		height={cardHeight}
-		style="border: 1px solid black"
-	/>
+	<div class="cutter">
+		<canvas
+			bind:this={canvas}
+			width={cardWidth}
+			height={cardHeight}
+			style="border: 1px solid black"
+		/>
+		<div class="control">
+			<h3>
+				Source Adjustment
+			</h3>
+			<div>
+				<input id="sourceOffsetX" type="number" bind:value={adjustSourceOffsetX} />
+				<label for="sourceOffsetX">Source offset X</label>
+			</div>
+			<div>
+				<input id="sourceOffsetX" type="number" bind:value={adjustSourceOffsetY} />
+				<label for="sourceOffsetX">Source offset Y</label>
+			</div>
+			<br/>
+			<h3>
+				Blank Title Adjustment
+			</h3>
+			<div>
+				<input id="displayBlankTitle" type="checkbox" bind:checked={isDisplayBlankTitle} />
+				<label for="displayBlankTitle">Display Blank Title</label>
+			</div>
+			<div>
+				<input id="blankTitleAdjustWidth" type="number" bind:value={adjustBlankTitleWidth}/>
+				<label for="blankTitleAdjustWidth">Title width adjustment</label>
+			</div>
+			<div>
+				<input id="blankTitleAdjustHeight" type="number" bind:value={adjustBlankTitleHeight}/>
+				<label for="blankTitleAdjustHeight">Title height adjustment</label>
+			</div>
+			<div>
+				<input id="blankTitleAdjustOffsetX" type="number" bind:value={adjustBlankTitleOffsetX}/>
+				<label for="blankTitleAdjustOffsetX">Title offset X</label>
+			</div>
+			<div>
+				<input id="blankTitleAdjustOffsetY" type="number" bind:value={adjustBlankTitleOffsetY}/>
+				<label for="blankTitleAdjustOffsetY">Title offset Y adjustment</label>
+			</div>
+			<div>
+				<input id="blankTitleFilterSapia" type="number" bind:value={adjustBlankTitleFilterSapia}/>
+				<label for="blankTitleFilterSapia">Title filter sapia</label>
+			</div>
+			<div>
+				<input id="blankTitleFilterSaturate" type="number" bind:value={adjustBlankTitleFilterSaturate}/>
+				<label for="blankTitleFilterSaturate">Title filter saturate</label>
+			</div>
+			<br/>
+			<h3>
+				Blank Dialog Adjustment
+			</h3>
+			<div>
+				<input id="displayBlankDialog" type="checkbox" bind:checked={isDisplayBlankDialog}/>
+				<label for="displayBlankDialog">Display blank dialog</label>
+			</div>
+			<div>
+				<input id="blankDialogAdjustWidth" type="number" bind:value={adjustBlankDialogWidth}/>
+				<label for="blankDialogAdjustWidth">Dialog width adjustment</label>
+			</div>
+			<div>
+				<input id="blankDialogAdjustHeight" type="number" bind:value={adjustBlankDialogHeight}/>
+				<label for="blankDialogAdjustHeight">Dialog height adjustment</label>
+			</div>
+			<div>
+				<input id="blankDialogAdjustOffsetX" type="number" bind:value={adjustBlankDialogOffsetX}/>
+				<label for="blankDialogAdjustOffsetX">Dialog offset X</label>
+			</div>
+			<div>
+				<input id="blankDialogAdjustOffsetY" type="number" bind:value={adjustBlankDialogOffsetY}/>
+				<label for="blankDialogAdjustOffsetY">Dialog offset Y adjustment</label>
+			</div>
+		</div>
+	</div>
 </div>
 
 <style lang="scss">
@@ -118,6 +273,16 @@ import { CardPositionStore } from '$lib/store';
 		flex-direction: column;
 		align-items: center;
 		row-gap: 0.5rem;
+
+		.cutter {
+			display: flex;
+			flex-direction: row;
+
+			.control {
+				display: flex;
+				flex-direction: column;
+			}
+		}
 		.navBtns {
 			display: flex;
 			column-gap: 1rem;
